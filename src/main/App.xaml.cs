50b7ad3d99a79aa2c959468ee3889dcbda04ff33
Elmer Roderick Niem Bool <35117496+elmerbool@users.noreply.github.com>
2023-04-02 22:30:12 +1000

    Update App.xaml.cs

--- src/main/App.xaml.cs
+++ src/main/App.xaml.cs
@@ -9,13 +9,4 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
-    protected override Window CreateWindow(IActivationState activationState)
-    {
-        Window window =base.CreateWindow(activationState);
-        window.Created += (e, s) =>
-        {
-            Console.WriteLine("PassXYZ.Vault.App: 1. Created event");
-        };
-		return window;
-    }
 }
